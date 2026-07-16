7874efa59c93033e7f3352f39762553464208ba6
Abdu Ahad <7249ahad@gmail.com>
2026-07-16 12:09:40 +0500

    Update .gitignore and refactor app.py for improved API key handling

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+api_key = st.secrets.get("GEMINI_API_KEY") or os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
 
 st.title("Streamlit First App")
-st.write("This app demonstrates a conversational agent. ")
+st.write("This app demonstrates a conversational agent.")
 
 user_input = st.text_input("Ask a question:")
+
 if st.button("Submit"):
-    with st.spinner("Agent is thinking..."):
-        response = client.models.generate_content(
-            model="gemini-2.5-flash" , contents=user_input
-        )
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=user_input
+    )
     st.write(response.text)
